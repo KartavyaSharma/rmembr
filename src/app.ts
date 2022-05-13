3dd644416db4ebfee34ae237b93931bbdc6ddff6
Kartavya Sharma <kartavya@berkeley.edu>
2022-05-13 09:57:49 -0700

    Added directory sturcture on local

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,32 @@
-import express from 'express';
+import express, { Express } from 'express';
 
-import routes from './routes';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+import routes from './routes/home';
+import cors = require('cors');
 
 class App {
-    public server;
+    public server:Express;
 
     constructor() {
         this.server = express();
         this.routes();
+        this.middleware();
     }
 
     routes() {
         this.server.use(routes);
     }
+
+    middleware() {
+        this.server.use(express.json());
+        this.server.use(helmet());
+        this.server.use(bodyParser.json());
+        this.server.use(cors());
+        this.server.use(morgan('combined'));
+    }
 }
+
 export default new App().server;
