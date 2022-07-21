e8fa61a15868f68dd75f606f872fcc8e880f06d2
Kartavya Sharma <kartavya@berkeley.edu>
2022-07-21 08:23:31 -0700

    Updated accessors and modifiers in subsection class

--- backend/src/rmembr/planner/subsection/subsection.ts
+++ backend/src/rmembr/planner/subsection/subsection.ts
@@ -33,6 +33,46 @@ export default class Subsection {
     /** Overall status of the current subsection. */
     private _status: IStatus;
 
+    public get id(): string {
+        return this._id;
+    }
+
+    public get sectionId(): string {
+        return this._sectionId;
+    }
+
+    public get name(): string {
+        return this._name;
+    }
+
+    public get inClass(): Date {
+        return this._inClass;
+    }
+
+    public get status(): IStatus {
+        return this._status;
+    }
+
+    public get revisionSchedule(): IRevisionSchedule {
+        return this._revisionSchedule;
+    }
+
+    public get plannedRevisionSchedule(): Date[] {
+        return this._plannedRevisionSchedule;
+    }
+
+    public get object(): ISubSection {
+        return {
+            _id: this._id,
+            _sectionId: this._sectionId,
+            name: this._name,
+            inClass: this._inClass,
+            revisionSchedule: this._revisionSchedule,
+            plannedRevisionSchedule: this._plannedRevisionSchedule,
+            status: this._status
+        }
+    }
+
     /** Initialize a new subsection ojbect. */
     constructor() {
         
@@ -48,7 +88,7 @@ export default class Subsection {
         return {} as IRevisionSchedule;
     }
 
-    public update(): ISubSection {
+    public update(newSubsection: ISubSection): ISubSection {
         // TODO: Implement
         return {} as ISubSection;
     }
@@ -58,18 +98,24 @@ export default class Subsection {
         return {} as ISubSection;
     }
     
+    public determineStatus(): IStatus {
+        // TODO: Implement
+        return {} as IStatus;
+    }
+    
+    private refreshSubsection(section: ISubSection): void {
+        // TODO: Implement
+        return;
+    }
+
     public static get(id: String): ISubSection {
         // TODO: Implement
         return {} as ISubSection;
     }
-
-    public static determineStatus(subsection: ISubSection): IStatus {
+    
+    public static refreshAll(): void {
         // TODO: Implement
-        return {} as IStatus;
+        return;
     }
 
-    public static refreshSubsection(section: ISubSection): ISubSection {
-        // TODO: Implement
-        return {} as ISubSection;
-    }
 }
