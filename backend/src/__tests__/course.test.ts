7ac2474f56eb5e2c2577d50269449e96b68419d3
Kartavya Sharma <kartavya@berkeley.edu>
2022-07-26 15:38:52 -0700

    Added course jest tests

--- backend/src/__tests__/course.test.ts
+++ backend/src/__tests__/course.test.ts
@@ -12,6 +12,12 @@ let userSetupBundle: { token: string, user: IUser };
 
 describe("Course Test", () => {
 
+    const newCourse = {
+        name: faker.lorem.word(),
+    }
+
+    let createdCourse: ICourse;
+
     beforeAll(async () => {
         app = await TestUtils.setupServer();
         supertestApp = supertest(app.server);
@@ -35,21 +41,19 @@ describe("Course Test", () => {
     });
 
     it("User adds a course to their course group", async () => {
-        const course = {
-            name: faker.lorem.word(),
-        }
         await supertestApp.
             post("/planner/courses").
             auth(userSetupBundle.token, { type: 'bearer' }).
-            send(course).
+            send(newCourse).
             expect(200).
             expect("Content-Type", /application\/json/).
             expect(async (res: any) => {
                 const payload: { course: ICourse } = res.body.payload;
+                createdCourse = payload.course;
                 expect(payload).toBeDefined();
                 expect(payload.course).toBeDefined();
                 expect(payload.course.name).toBeDefined();
-                expect(payload.course.name).toEqual(course.name);
+                expect(payload.course.name).toEqual(newCourse.name);
                 expect(payload.course._courseGroupId).toEqual(
                     (await User.getUser(userSetupBundle.user.email)).courseGroupId
                 );
@@ -63,6 +67,45 @@ describe("Course Test", () => {
             send({}).
             expect(400).
             expect("Content-Type", /application\/json/);
+    });
+
+    it("User updates a course name to something else", async () => {
+        const updatedCourse: ICourse = {
+            _id: createdCourse._id,
+            _courseGroupId: createdCourse._courseGroupId,
+            name: faker.lorem.words(),
+            sections: createdCourse.sections
+        }
+        await supertestApp.
+            patch(`/planner/courses/${createdCourse._id}`).
+            auth(userSetupBundle.token, { type: 'bearer' }).
+            send({ course: updatedCourse }).
+            expect(200).
+            expect("Content-Type", /application\/json/).
+            expect(async (res: any) => {
+                const payload = res.body.payload;
+                expect(payload).toBeDefined();
+                expect(payload._id).toBeDefined();
+                expect(payload._courseGroupId).toBeDefined();
+                expect(payload.sections).toBeDefined();
+                expect(payload.name).toEqual(updatedCourse.name);
+                expect(payload._id).toEqual(updatedCourse._id);
+                expect(payload._courseGroupId).toEqual(updatedCourse._courseGroupId);
+            });
+    });
+
+    it("User deletes a course form their course group", async () => {
+        await supertestApp.
+            delete(`/planner/courses/${createdCourse._id}`).
+            auth(userSetupBundle.token, { type: 'bearer' }).
+            expect(200).
+            expect("Content-Type", /application\/json/).
+            expect(async (res: any) => {
+                const payload = res.body.payload;
+                expect(payload).toBeDefined();
+                expect(payload.courses).toBeDefined();
+                expect(payload.courses.length).toEqual(0);
+            });
     });
 
     afterAll(async () => {
