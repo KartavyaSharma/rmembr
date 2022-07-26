ed11f0ec80f20fb6dd1a6b3a5dec09dbd18ed3e5
Kartavya Sharma <kartavya@berkeley.edu>
2022-07-25 20:30:45 -0700

    testing workflow

--- backend/src/utils/database/db.ts
+++ backend/src/utils/database/db.ts
@@ -52,6 +52,7 @@ export class Db {
             });
         }
         try {
+            console.log(config.uri);
             if (mongoose.connection.readyState !== 1 && mongoose.connection.readyState !== 2) {
                 this._instance = await mongoose.connect(
                     config.uri,
